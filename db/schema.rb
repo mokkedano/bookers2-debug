50193b7490ff1e595c0393ecbdd8a80c45aa9f68
mokkedano <126681793+mokkedano@users.noreply.github.com>
2023-03-23 16:35:36 +0000

    [Add] like option

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_23_054437) do
+ActiveRecord::Schema.define(version: 2023_03_23_155356) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -50,9 +50,9 @@ ActiveRecord::Schema.define(version: 2023_03_23_054437) do
 
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
-    t.integer "post_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "book_id"
   end
 
   create_table "users", force: :cascade do |t|
