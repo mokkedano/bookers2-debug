acd94be67ef13e2dc91719238e4a2c8ed4baf159
mokkedano <126681793+mokkedano@users.noreply.github.com>
2023-03-28 13:47:33 +0000

    [Add] search option

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,7 +28,7 @@ class User < ApplicationRecord
   end
 
 
-
+# フォロー・フォロワー機能
   def follow(user_id)
     relationships.create(followed_id: user_id)
   end
@@ -40,5 +40,22 @@ class User < ApplicationRecord
   def following?(user)
     followings.include?(user)
   end
+  
+  
+# 検索機能
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @user = User.where("name LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @user = User.where("name LIKE?", "#{word}%")
+    elsif search == "backward_match"
+      @user = User.where("name LIKE?", "%#{word}")
+    elsif search == "partial_match"
+      @user = User.where("name LIKE?", "%#{word}%")
+    else 
+      @user = User.all
+    end
+  end
+  
 
 end
