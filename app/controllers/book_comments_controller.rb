1b743322ec3a855c49327bcaded69bf3ac9c4a94
mokkedano <126681793+mokkedano@users.noreply.github.com>
2023-04-01 12:52:16 +0000

    [Finish] Ajax

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,16 +1,19 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
+    @book = Book.find(params[:book_id])
+    comment = BookComment.new(book_comment_params)
+    comment.user_id = current_user.id
+    comment.book_id = @book.id
     comment.save
-    redirect_back(fallback_location: root_path)
+    @book_comment = BookComment.new
   end
 
   def destroy
-    BookComment.find(params[:id]).destroy
-    redirect_back(fallback_location: root_path)
+    @book = Book.find(params[:book_id])
+    @book_comment = BookComment.find(params[:id])
+    @book_comment.destroy
+    @book_comment = BookComment.new
   end
 
 
