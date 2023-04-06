9cf5d6f1713d5edb38de0a6662f2fe14e32afc36
mokkedano <126681793+mokkedano@users.noreply.github.com>
2023-04-06 13:08:47 +0000

    [Add] group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,33 +1,65 @@
 class GroupsController < ApplicationController
-  
-  
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:edit, :update]
+
+
+
   def new
     @group = Group.new
   end
-  
-  
+
+
   def create
-end
-  
-  
-  
-  def index
-    @book = Book.new
-    @groups = Group.all
+    @group = Group.new(group_params)
+    @group.owner_id = current_user.id
+    if @group.save
+      redirect_to groups_path
+    else
+      render 'new'
+    end
   end
-  
-  
+
+
+  def index
+    @groups = Group.all
+    @book = Book.new
+  end
+
+
+  def show
+    @group = Group.find(params[:id])
+    @book = Book.new
+  end
+
+
   def edit
     @group = Group.find(params[:id])
   end
-  
-  
-  
-  private
-  
-  def group_params
-    params.require(:group).permit(:name, :introduction, :image_id)
+
+
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to groups_path
+    else
+      render "edit"
+    end
   end
-  
-  
+
+
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, :introduction, :group_image)
+  end
+
+
+  def ensure_correct_user
+    @group = Group.find(params[:id])
+    unless @group.owner_id == current_user.id
+      redirect_to groups_path
+    end
+  end
+
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -13,7 +13,7 @@ class Group < ApplicationRecord
 
   def get_group_image(width, height)
     unless group_image.attached?
-      file_path = Rails.root.join('app/assets/images/no-image-icon.jpg')
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
       group_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
     group_image.variant(resize_to_limit: [width, height]).processed
