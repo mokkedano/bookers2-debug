ad820ae36f6865cfa5291bb1c4b82c25a77f360c
mokkedano <126681793+mokkedano@users.noreply.github.com>
2023-06-25 07:37:45 +0000

    [Add]応用課題7a

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,8 +13,10 @@ class BooksController < ApplicationController
     from = (to - 6.day).at_beginning_of_day
     @books = Book.includes(:favorited_users).
       sort {|a,b|
-        b.favorited_users.includes(:favorites).where(created_at: from...to).size <=>
-        a.favorited_users.includes(:favorites).where(created_at: from...to).size
+        # b.favorited_users.includes(:favorites).where(created_at: from...to).size <=>
+        # a.favorited_users.includes(:favorites).where(created_at: from...to).size
+        b.favorites.where(created_at: from...to).size <=>
+        a.favorites.where(created_at: from...to).size
       }
     @book = Book.new
   end
