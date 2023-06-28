4da84db7bca85e41e98e4dd79fa287d9c386c8e2
mokkedano <126681793+mokkedano@users.noreply.github.com>
2023-06-28 13:46:30 +0000

    [Add] 応用課題7b

--- app/models/book.rb
+++ app/models/book.rb
@@ -9,6 +9,13 @@ class Book < ApplicationRecord
   validates :body, presence: true, length: { maximum:200 }
 
 
+  # 投稿数表示のための記述
+  scope :created_today, -> { where(created_at: Time.zone.now.all_day) }
+  scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) }
+  scope :created_this_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) }
+  scope :created_last_week, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day) }
+
+
 # いいね機能
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
